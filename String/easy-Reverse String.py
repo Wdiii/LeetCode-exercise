0d18bc26c8481df15c55e7bdd995ad84c44962a8
Di W <48701693+Wdiii@users.noreply.github.com>
2020-05-03 21:17:10 -0700

    Add files via upload

---
+++ String/easy-Reverse String.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+class Solution:
+    def reverseString(self, s):
+        #: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
+                
