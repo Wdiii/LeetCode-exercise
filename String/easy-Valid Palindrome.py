069f7836a4aa3ac3c362f824fa996dd183ad8b92
Di W <48701693+Wdiii@users.noreply.github.com>
2020-05-03 21:13:52 -0700

    Update easy-Valid Palindrome.py

--- String/easy-Valid Palindrome.py
+++ String/easy-Valid Palindrome.py
@@ -7,10 +7,10 @@ This is a temporary script file.
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        char_num=list(filter(str.isalnum, s.lower())) #get character and number
-        for i in range(len(char_num)//2):
+        char_num=list(filter(str.isalnum, s.lower()))
+        res=True
+        for i in range(len(char_num)-1):
             if char_num[i]!=char_num[len(char_num)-1-i]:
+                res=False
                 break
-                return False
-            else:
-                return True   
+        return res
