366aad46f0098fcc3e22aae506f47fef8e218b3b
Di W <48701693+Wdiii@users.noreply.github.com>
2020-05-04 23:28:19 -0700

    Add files via upload

---
+++ String/easy-Number of Segments in a String.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+class Solution:
+    def countSegments(self, s: str) -> int:
+        return len(s.split())
+            
