0c3b41805fa719e85552fb2599d6863baacf82b7
Di W <48701693+Wdiii@users.noreply.github.com>
2020-05-06 00:00:33 -0700

    Add files via upload

---
+++ String/easy-Repeated Substring Pattern.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        n=len(s)
+        for i in range(1,n//2+1):
+            if n%i==0:
+                a=s[:i]
+                j=i
+                while j<n and s[j:j+i]==a:
+                    j+=i
+                if j==n:
+                    return True
+        return False
