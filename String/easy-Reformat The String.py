720687e5ec5357b92cebfcbb9122b13273f9d1e7
Di W <48701693+Wdiii@users.noreply.github.com>
2020-05-13 23:45:40 -0700

    Add files via upload

---
+++ String/easy-Reformat The String.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+class Solution:
+    def reformat(self, s: str) -> str:
+        num="".join(list(filter(str.isdigit,s)))
+        char="".join(list(filter(str.isalpha,s)))
+        if abs(len(num)-len(char))>1:
+            res=""
+        elif len(num)==len(char):
+            res=""
+            for i in range(len(num)):
+                res+="".join(num[i]+char[i])
+        elif len(num)-len(char)==1:
+            res=num[-1]
+            for i in range(len(char)):
+                res+="".join(char[i]+num[i])
+        else:
+            res=char[-1]
+            for i in range(len(num)):
+                res+="".join(num[i]+char[i])
+        return res
