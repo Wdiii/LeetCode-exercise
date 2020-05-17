b3b9d26cf34bfd64399b4cfd453a51802f68e7f0
Di W <48701693+Wdiii@users.noreply.github.com>
2020-05-16 22:05:17 -0700

    Add files via upload

---
+++ Math/easy-Sum of Square Numbers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import math
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        left=0
+        right=int(math.sqrt(c))
+        while left<=right:
+            if left**2+right**2<c:
+                left+=1
+            elif left**2+right**2>c:
+                right-=1
+            else:
+                return True
+        return False
