9b80f1dff6d34832e276a7842b24831db037569f
Di W <48701693+Wdiii@users.noreply.github.com>
2020-05-20 00:45:16 -0700

    Update easy-Powerful Integers.py

--- Math/easy-Powerful Integers.py
+++ Math/easy-Powerful Integers.py
@@ -31,3 +31,23 @@ class Solution:
                     b=0
         return list(set(res))
       
+#easier way
+class Solution1:
+    def powerfulIntegers(self, x, y, bound):
+        i = 0
+        j = 0
+        A = set()
+        x, y = max(x,y), min(x,y)
+        while x**i + y**j <= bound:
+            if x == 1:
+                return [2]
+            if y == 1:
+                A.add(x**i+1)
+            else:
+                while x**i + y**j <= bound:
+                    A.add(x**i+y**j)
+                    j+=1
+            i+=1
+            j=0
+        return list(A)
+      
