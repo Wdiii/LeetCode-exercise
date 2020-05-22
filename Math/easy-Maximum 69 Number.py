7e3c9b01c10c52e826d626175cf94177eac06cb0
Di W <48701693+Wdiii@users.noreply.github.com>
2020-05-21 21:42:32 -0700

    Add files via upload

---
+++ Math/easy-Maximum 69 Number.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+           
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        N=str(num).replace("6","9",1)
+        return int(N)
