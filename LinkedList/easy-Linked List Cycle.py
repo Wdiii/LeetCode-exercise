6e847f2d90805887d0f464e032e02b2ccb17a95f
Di W <48701693+Wdiii@users.noreply.github.com>
2020-05-27 01:00:03 -0700

    Add files via upload

---
+++ LinkedList/easy-Linked List Cycle.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# Definition for singly-linked list.
+class ListNode:
+   def __init__(self, x):
+       self.val = x
+       self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        p1=p2=head
+        while p1 and p1.next:
+            p1=p1.next.next
+            p2=p2.next
+            if p1==p2:
+                return True
+        return False
+    
