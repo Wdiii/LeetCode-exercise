e8a5c06388ad5a4fd957a14dd2b8ef0356258d28
Di W <48701693+Wdiii@users.noreply.github.com>
2020-05-26 21:56:08 -0700

    Add files via upload

---
+++ LinkedList/easy-Remove Duplicates from Sorted List.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        p=head
+        while p and p.next:
+            if p.val==p.next.val:
+                p.next=p.next.next
+            else:
+                p=p.next
+        return head
