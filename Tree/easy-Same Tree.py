ebef1af8a719c0ed8dcfdc92ebda07e372457434
Di W <48701693+Wdiii@users.noreply.github.com>
2020-06-12 20:53:41 -0700

    Create easy-Same Tree.py

---
+++ Tree/easy-Same Tree.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p, q):
+        #p: TreeNode, q: TreeNode) -> bool:
+        if p == None or q == None:
+            return p == q
+        elif p.val == q.val:
+            return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
+        else:
+            return False
