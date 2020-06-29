50534e2064fa7048209073ad602299b8e1e37094
Di W <48701693+Wdiii@users.noreply.github.com>
2020-06-28 23:13:22 -0700

    Add files via upload

---
+++ Array/medium-Find All Duplicates in an Array.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+class Solution:
+    def findDuplicates(self, nums):
+        #nums: List[int]) -> List[int]:
+        res= []
+        for i in nums:
+            if nums[abs(i)-1]<0:
+                res.append(abs(i))
+            else:
+                nums[abs(i)-1]=-nums[abs(i)-1]
+        return res
+#do it without extra space
