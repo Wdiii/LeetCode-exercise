a7e4e2d7bd76f7724c660a114801bb8d95df3c5b
Di W <48701693+Wdiii@users.noreply.github.com>
2020-07-19 00:18:54 -0700

    Update medium-Can Make Palindrome from Substring.py

--- Array/medium-Can Make Palindrome from Substring.py
+++ Array/medium-Can Make Palindrome from Substring.py
@@ -27,31 +27,31 @@ class Solution:
 
 class Solution1:
     def canMakePaliQueries(self, s, queries):
-        alphabet = set(s) #无需处理26个字母，只需处理在s中出现过的字母
-        dic = dict()
+        alphabet=set(s) #无需处理26个字母，只需处理在s中出现过的字母
+        dic={}
         for char in alphabet:
-            dic[char] = [0 for _ in range(len(s))]
-        for i, ch in enumerate(s): #计算出现次数的前缀和
+            dic[char]=[0 for _ in range(len(s))]
+        for i,ch in enumerate(s): #计算出现次数的前缀和
             for char in alphabet:
-                if ch == char:
-                    if i > 0:
-                        dic[ch][i] = dic[ch][i - 1] + 1
+                if ch==char:
+                    if i>0:
+                        dic[ch][i]=dic[ch][i-1]+1
                     else:
                         dic[ch][i] = 1
                 else:
-                    if i > 0:
-                        dic[char][i] = dic[char][i - 1]             
+                    if i>0:
+                        dic[char][i]=dic[char][i-1]             
         res = []
-        for left, right, k in queries:
-            odd_cnt = 0
+        for left,right,k in queries:
+            odd=0
             for char in alphabet:
-                if left > 0:
-                    if (dic[char][right] - dic[char][left - 1]) % 2 == 1:# 直接相减得到字母出现次数
-                        odd_cnt += 1
+                if left>0:
+                    if (dic[char][right]-dic[char][left-1])%2:# 直接相减得到字母出现次数
+                        odd+=1
                 else:
-                    if dic[char][right] % 2 == 1:
-                        odd_cnt += 1
-            if odd_cnt // 2 <= k:
+                    if dic[char][right]%2:
+                        odd+=1
+            if odd//2 <= k:
                 res.append(True)
             else:
                 res.append(False)                
